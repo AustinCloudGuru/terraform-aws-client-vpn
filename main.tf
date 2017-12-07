d158fc376f4ed0d2303d44289ef68d5465236f18
Mark Honomichl <mars@marsdominion.com>
2017-12-07 13:56:49 -0600

    Updated to be less dependent of regions

--- main.tf
+++ main.tf
@@ -9,9 +9,9 @@ data "terraform_remote_state" "aws-base" {
 
 module "client-vpn" {
   source                   = "./modules/client-vpn"
-  vpn-1a_create           = true
-  vpn-1a_subnet_cidr      = "${var.vpn-1_subnet_cidr}"
+  vpn-1_create           = true
+  vpn-1_subnet_cidr      = "${var.vpn-1_subnet_cidr}"
   vpc_id                   = "${data.terraform_remote_state.aws-base.vpc_id}"
-  nat-gw-1a                = "${data.terraform_remote_state.aws-base.nat-gw-1}"
+  nat-gw-1                = "${data.terraform_remote_state.aws-base.nat-gw-1}"
   internet-gateway         = "${data.terraform_remote_state.aws-base.internet-gateway}"
 }
--- modules/client-vpn/vpn-instance.tf
+++ modules/client-vpn/vpn-instance.tf
@@ -1,9 +1,18 @@
+data "aws_ami" "amazon_ami" {
+  most_recent = true
+  owners = ["aws-marketplace"]
+  filter {
+    name = "product-code"
+    values = ["aw0evgkw8e5c1q413zgy5pjce"]
+  }
+}
+
 resource "aws_instance" "vpn" {
-  ami = "ami-6d1c2007"
+  ami = "${data.aws_ami.amazon_ami.image_id}"
   instance_type = "t2.medium"
   associate_public_ip_address = true
   subnet_id = "${aws_subnet.vpn-1.id}"
-  key_name = "bosh"
+  key_name = "ml-aws-prototype"
   vpc_security_group_ids = ["${aws_security_group.vpn.id}"]
   user_data = "${file("${path.module}/bootstrap.sh")}"
   tags {
